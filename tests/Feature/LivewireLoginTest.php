42bdc395e4504bdd854daefcb1bb138d814c8494
onomatopoeia69 <carljameslangres@gmail.com>
2025-07-17 12:57:36 +0800

    adding test for admin login

--- tests/Feature/LivewireLoginTest.php
+++ tests/Feature/LivewireLoginTest.php
@@ -31,6 +31,25 @@ class LivewireLoginTest extends TestCase
         $this->assertAuthenticatedAs($user);
     }
 
+       public function test_admin_can_login_and_access_admin_dashboard()
+    {
+
+        $admin = User::factory()->create([
+                    'password' => bcrypt('password123'),
+                    'role' => 'admin', 
+                ]);
+        
+         Livewire::test('auth.login')
+            ->set('email', $admin->email)
+            ->set('password', 'password123')
+            ->call('inputLogin')
+            ->assertRedirect('/admin/dashboard');
+
+         $this->assertAuthenticatedAs($admin);
+    }
+
+
+
     public function test_user_cannot_login_with_wrong_credentials()
     {
          $user = User::factory()->create();
@@ -60,6 +79,8 @@ class LivewireLoginTest extends TestCase
         ->assertSet('cooldown', fn ($value) => $value > 0);
 
     }
+
+   
 
 
 
